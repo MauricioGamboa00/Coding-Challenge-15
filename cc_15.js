9b9480551a1b6b26f31922c3ec4fd6beb67c06f2
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-03-16 14:10:50 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,7 @@
+// Task 1 Creating the Base Structure
+
+document.addEventListener('DOMContentLoaded', function() {
+    const riskDashboard = documentgetbyID("riskDashboard");
+    console.log("Risk Dashboard Loaded");
+}); // When the document is loaded sucessfully log a message to the console
+
