e8095308bf1562c975681249a93a6286822d6f51
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-03-16 15:15:46 -0400

    Task 3 - Removing Risk Items

--- cc_15.js
+++ cc_15.js
@@ -23,11 +23,22 @@ function addRiskItem(riskName, riskLevel, department){
     const label = document.createElement('label'); // Creating a label as the department name
     label.textContent = `Department: ${department}`;
 
+    // Creating Resolve Button for Task 3
 
+    const resolveButton = document.createElement('button'); // Creating Resolve Button
+    resolveButton.textContent = "Resolve"; 
+
+    // Creating Event Listener for Resolve Button so that it removes a risk card when the button is clicked and logs a message to the console
+
+    resolveButton.addEventListener('click', () => {
+        console.log(riskName, "Risk Card Removed.")
+        riskCard.remove();
+    });
 
     riskCard.appendChild(heading); // appending the Heading
     riskCard.appendChild(paragraph); // Appending the Paragraph
     riskCard.appendChild(label); // Appending the Label
+    riskCard.appendChild(resolveButton); // Appending the resolve button
 
 
     riskDashboard.appendChild(riskCard); // Appending riskCard to riskDashboard
@@ -42,8 +53,7 @@ document.getElementById('newRiskBtn').addEventListener('click', () => { // Added
     const riskLevel = riskLevelInput.value; // Returns input entered in Risk Level
     const department = departmentInput.value; // Returns input entered in Department
 
-    addRiskItem(riskName, riskLevel, department); // calling add risk item fucntion to create a new risk card in the risk dashboard
-
+    addRiskItem(riskName, riskLevel, department); // calling add risk item function to create a new risk card in the risk dashboard
     
 })
 
@@ -52,3 +62,6 @@ document.getElementById('newRiskBtn').addEventListener('click', () => { // Added
 
 addRiskItem("Data Breach", "High", "IT"); // Adding a new risk item
 addRiskItem("Supply Chain Disruption", "Medium", "Operations"); // Adding a new risk item
+
+// Task 3 Removing Risk Items 
+
